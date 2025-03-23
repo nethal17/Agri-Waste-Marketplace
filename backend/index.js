e82edd4ee76b6b3ea09c3ea6c2fcbbd89fe16b1a
Ricky Perera <pereraricky20@gmail.com>
2025-03-24 01:15:59 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -52,15 +52,9 @@ app.use('/api/reviews', reviewRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/products', productRoutes);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 //Naduli
 app.use("/api/agri-waste", agriWasteRoutes);
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
